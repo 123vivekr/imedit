34b9e2e40624bfe785126120e882ca08348c1f95
123vivekr <123vivekr@gmail.com>
2022-01-03 12:04:27 +0530

    Add. test for image resize function

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,25 @@ pub fn resize(img: Vec<u8>, width: usize, height: usize) -> Vec<u8> {
     let mut result_vector = Vec::new();
     let (width, height) = rgb_image.dimensions();
     let png_encoder = image::codecs::png::PngEncoder::new(&mut result_vector);
-    png_encoder.encode(&rgb_image, width as u32, height as u32, image::ColorType::Rgb8);
+    png_encoder.encode(&rgb_image, width as u32, height as u32, image::ColorType::Rgb8).unwrap();
     result_vector
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_return_resized_image() {
+        let (want_width, want_height) = (200, 100);
+
+        let sample_image_filename = "sample_image.png";
+        let image_vector = std::fs::read(sample_image_filename).unwrap();
+
+        let resized_image_vector = resize(image_vector, want_width, want_height);
+
+        let result = image::load_from_memory_with_format(&resized_image_vector, image::ImageFormat::Png).unwrap();
+        let (got_width, got_height) = result.to_rgba8().dimensions();
+        assert_eq!((want_width, want_height), (got_width as usize, got_height as usize));
+    }
 }
